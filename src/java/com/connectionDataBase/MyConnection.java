367774a1e5cee8e10f538e5d0d1b4c6f5e7a1091
Shubham <shubham@gmail.com>
2026-04-28 13:32:29 +0530

    again update

--- src/java/com/connectionDataBase/MyConnection.java
+++ src/java/com/connectionDataBase/MyConnection.java
@@ -12,15 +12,22 @@ public class MyConnection {
             String dbUrl = System.getenv("DB_URL");
             String dbUser = System.getenv("DB_USER");
             String dbPass = System.getenv("DB_PASSWORD");
+            
+            // Debug print
+            System.out.println("Runtime DB_URL = " + dbUrl);
+            System.out.println("Runtime DB_USER = " + dbUser);
 
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             if (dbUrl != null) {
                 con = DriverManager.getConnection(dbUrl, dbUser, dbPass);
+                System.out.println("Connection established successfully!");
             }
         } catch (Exception e) {
             e.printStackTrace();
+            
         }
+        
         return con;
     }
 }
